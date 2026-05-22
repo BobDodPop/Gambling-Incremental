fe5ddaf6ab3d94c41b120461de3e05ca5c554a08
BobDodPop <165052745+BobDodPop@users.noreply.github.com>
2026-05-22 09:41:55 -0500

    Implement basic gambling game logic
    Added basic gambling functionality with random points allocation.

--- Gambling.py
+++ Gambling.py
@@ -1,1 +1,18 @@
+import random
 
+points = 100
+gambling = True
+result = 0
+min_roll = 1
+max_roll = 100
+
+print("Press enter to gamble")
+print("Type guide for instructions")
+
+while gambling? = True:
+	roll = input("")
+  if roll == "":
+		roll = "."
+		result = random.randint(min_roll, max_roll)
+		points += result/100
+		print("You gained", result/100, "points!"
