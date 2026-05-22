868e44a24f2bc4593da3297f9e7709fff6a3ce43
BobDodPop <165052745+BobDodPop@users.noreply.github.com>
2026-05-22 09:47:17 -0500

    Refactor gambling loop and implement gamble function
    Fix gambling logic and add gamble function

--- Gambling.py
+++ Gambling.py
@@ -6,13 +6,23 @@ result = 0
 min_roll = 1
 max_roll = 100
 
+def gamble:
+	choice = "."
+	result = random.randint(min_roll, max_roll)
+	points += result/100
+	print("You gained", result/100, "points!")
+	
+
 print("Press enter to gamble")
 print("Type guide for instructions")
 
-while gambling? = True:
-	roll = input("")
-  if roll == "":
-		roll = "."
-		result = random.randint(min_roll, max_roll)
-		points += result/100
-		print("You gained", result/100, "points!"
+while gambling == True:
+	choice = input("").lower()
+	if choice == "":
+		gamble()
+	if choice == "guide":
+		print("Press enter to gamble")
+		print("Type guide for instructions")
+		print("Type i to view inventory")
+		print("Type u to view upgrades")
+		print("Type b 'upgrade name' 'amount' to buy an upgrade")
